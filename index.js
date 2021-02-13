126d4027d8858564d6c61fd8e9a15472e2dfaabb
shahoz123 <79020629+shahoz123@users.noreply.github.com>
2021-02-13 22:47:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/EXCuvNd`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/tyH6b6zwGB`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("755636700482633791").send(
+  client.channels.get("806313421947404328").send(
 `> <:ARBELE_UPUP:791411050238836766> By: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
@@ -51,10 +51,10 @@ ${message.content}`)
 client.on('ready', () => {
 console.log(`ON ${client.guilds.size} Servers`);
 console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`ڕیکلامەکە بنێرە بۆمن دایدەنێم☑️`) 
+client.user.setGame(`FIRE STAF REKLAM`) 
 });
 
 
 
 
-client.login("");//تۆکین لێرە دانێ
+client.login("ODA5OTQ2NzUxNDc3NDgxNTAz.YCcfsQ.OTjdCL09qh_H3q4I1xRF1CUIZBg");//تۆکین لێرە دانێ
